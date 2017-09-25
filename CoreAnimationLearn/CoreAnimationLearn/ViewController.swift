05bbe573f67de8ff45666ddd074da3957e311c62
alanzhangg <alanzhangg@icloud.com>
2017-09-25 18:13:48 +0800

    add ..

--- CoreAnimationLearn/CoreAnimationLearn/ViewController.swift
+++ CoreAnimationLearn/CoreAnimationLearn/ViewController.swift
@@ -12,15 +12,27 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mainView: UIView!
     var mainLayer: CALayer!
+	let secView :UIView = UIView(frame: CGRect(x: 20, y: 20, width: 70, height: 70))
+	let backView : UIView = UIView(frame: CGRect(x: 20, y: 20, width: 70, height: 70))
+	let transformView : UIView = UIView(frame: CGRect(x: 20, y: 20, width: 70, height: 70))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		view.addSubview(backView)
+		view.addSubview(secView)
+		
+		secView.backgroundColor = UIColor.yellow
+		backView.backgroundColor = UIColor.purple
         mainLayer = CALayer()
         mainLayer.backgroundColor = UIColor.blue.cgColor
         mainLayer.bounds = CGRect(x: 0.0, y: 0.0, width: 70, height: 70)
-//        mainLayer.position = CGPoint(x: 55.0, y: UIScreen.main.bounds.height - 55.0)
         mainLayer.position = CGPoint(x: UIScreen.main.bounds.width - 55.0, y: UIScreen.main.bounds.height - 55.0)
         view.layer.addSublayer(mainLayer)
+		print(mainLayer.convert(CGPoint(x: 50.0, y: 50.0), to: view.layer))
+		
+		mainView.addSubview(transformView)
+		transformView.backgroundColor = UIColor.blue
+		
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -29,7 +41,55 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+	@IBAction func pauseAnimation(_ sender: Any) {
+		let pausedTime = secView.layer.convertTime(CACurrentMediaTime(), from: nil)
+		secView.layer.speed = 0.0
+		secView.layer.timeOffset = pausedTime
+	}
+	@IBAction func transformAnimation(_ sender: Any) {
+		var perspective = CATransform3DIdentity
+		perspective.m34 = -1.0 // eyepositon
+		transformView.layer.sublayerTransform = perspective
+	}
+	
+	@IBAction func resumeAnimation(_ sender: Any) {
+		let pausedTime = secView.layer.timeOffset
+		secView.layer.speed = 1.0
+		secView.layer.timeOffset = 0.0
+		secView.layer.beginTime = 0.0
+		let timeSincePause = secView.layer.convertTime(CACurrentMediaTime(), from: nil)
+		secView.layer.beginTime = timeSincePause
+	}
+	
+	@IBAction func viewTransitionAnimation(_ sender: Any) {
+		let transition = CATransition()
+		transition.startProgress = 0.0
+		transition.endProgress = 1.0
+		transition.type = kCATransitionPush
+		transition.subtype = kCATransitionFromBottom
+		transition.duration = 0.2
+		
+		secView.layer.add(transition, forKey: "transition")
+		backView.layer.add(transition, forKey: "transition")
+		
+		secView.isHidden = true
+		backView.isHidden = false
+		
+	}
+	
+	@IBAction func viewLayerCoreAnimation(_ sender: Any) {
+		let newPoint = CGPoint(x: 100, y: 100)
+		UIView.animate(withDuration: 1.0) {
+			self.secView.layer.opacity = 0.5
+			let animation = CABasicAnimation(keyPath: "position")
+			animation.fromValue = NSValue(cgPoint: self.secView.layer.position)
+			animation.toValue = NSValue(cgPoint: newPoint)
+			animation.duration = 5.0
+			self.secView.layer.add(animation, forKey: "AnimateFrame")
+		}
+		self.secView.layer.position = newPoint
+	}
+	
     @IBAction func keyframeAnimation(_ sender: Any) {
         let thePath = CGMutablePath()
         thePath.move(to: CGPoint(x: 55.0, y: UIScreen.main.bounds.height - 55.0))
@@ -58,7 +118,6 @@ class ViewController: UIViewController {
         let widthValues = [1.0, 10.0, 5.0, 30.0, 0.5, 15.0, 2.0, 50.0, 0.0]
         Animi1.values = widthValues
         Animi1.calculationMode = kCAAnimationPaced
-        
         //Animation 2
         let Anima2 = CAKeyframeAnimation(keyPath: "borderColor")
         let colorValue = [UIColor.green.cgColor, UIColor.red.cgColor, UIColor.blue.cgColor]
